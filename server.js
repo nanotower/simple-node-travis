f285ff45edf9e41f04879c05c9e72750accee6df
nanotower <fernandodelatorre4@gmail.com>
2021-04-25 10:58:43 +0200

    print env var

--- server.js
+++ server.js
@@ -2,11 +2,12 @@ function sleep(ms) {
     return new Promise(resolve => setTimeout(resolve, ms));
   }
   
+  const favoriteFood = process.env.FAVORITE_FOOD;
+  console.log(`My favorite food is ${favoriteFood}`);
+  
   async function main() {
     while(true) {
       console.log('Containers rule!');
-      const favoriteFood = process.env.FAVORITE_FOOD;
-      console.log(`My favorite food is ${favoriteFood}`);
       await sleep(5000);
     }
   }
